a8d042d4d585f0566552fb09357bbe0a3b6fa8ed
Rao Fahad <raofahad046@gmail.com>
2023-10-23 12:20:02 +0500

    Initial commit

---
+++ test/crud-api-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrudApiDynamodb from '../lib/crud-api-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/crud-api-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrudApiDynamodb.CrudApiDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
