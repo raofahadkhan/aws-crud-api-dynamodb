2c65a1a05de4f7d451db9958351ec59f51649097
RAO FAHAD <raofahad046@gmail.com>
2023-12-10 01:11:33 +0500

    removed unwanted code from stream lambda

--- lambda/StreamLambda.ts
+++ lambda/StreamLambda.ts
@@ -4,8 +4,13 @@ import { DynamoDBStreamEvent } from "aws-lambda";
 const s3 = new AWS.S3();
 export const handler = async (event: DynamoDBStreamEvent) => {
   const bucketName = process.env.BUCKET_NAME!;
-  console.log("Event ===>", event);
-  console.log("data returned by dynamodb stream ===>", event.Records[0].dynamodb?.NewImage);
+
+  if (!bucketName) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Bucket Name not provided!" }),
+    };
+  }
 
   for (const record of event.Records) {
     const data = JSON.stringify(record.dynamodb!.NewImage);
@@ -19,4 +24,9 @@ export const handler = async (event: DynamoDBStreamEvent) => {
 
     await s3.putObject(params).promise();
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Data Added to S3 Successfully!" }),
+  };
 };
